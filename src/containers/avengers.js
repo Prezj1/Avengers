58a143c354e3f4d6f35b42f457927953ebcc9714
Prezj1 <joymakasi@hotmail.co.uk>
2021-03-16 13:23:31 +0000

    changing names

--- src/containers/avengers.js
+++ src/containers/avengers.js
@@ -1,49 +1,49 @@
 export const avengers = [
   {
     id: 1,
-    name: 'Bruce Banner',
+    name: 'The Hulk',
     username: 'Hulk',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/83.jpg?v=1602236687',
     link: 'https://marvelcinematicuniverse.fandom.com/wiki/Hulk'
   },
   {
     id: 2,
-    name: 'Steve Rogers',
+    name: 'Captain America',
     username: 'Cap',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/274.jpg?v=1599853488',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Captain_America"
   },
   {
     id: 3,
-    name: 'Clint Barton',
+    name: 'Hawkeye',
     username: 'Hawkeye',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/73.jpg?v=1597500181',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Hawkeye"
   },
   {
     id: 4,
-    name: 'Natasha Romanov',
+    name: 'Black Widow',
     username: 'Black Widow',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/248.jpg?v=1595053219',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Black_Widow"
   },
   {
     id: 5,
-    name: 'Wanda Maximoff',
+    name: 'Scarlet Witch',
     username: 'Wanda',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/444.jpg?v=1595433035',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Scarlet_Witch"
   },
   {
     id: 6,
-    name: 'Sam Wilson',
+    name: 'Falcon',
     username: 'Falcon',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/56.jpg?v=1599722900',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Falcon"
   },
   {
     id: 7,
-    name: 'Bucky Barnes',
+    name: 'Winter Soldier',
     username: 'Bucky',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/10027.jpg?v=1600577767',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Winter_Soldier"
@@ -64,7 +64,7 @@ export const avengers = [
   },
   {
     id: 10,
-    name: 'Stephen Strange',
+    name: 'Doctor Strange',
     username: 'Doc',
     icon: 'https://www.superherodb.com/pictures2/portraits/10/050/55.jpg?v=1607072096',
     link: "https://marvelcinematicuniverse.fandom.com/wiki/Doctor_Strange"
